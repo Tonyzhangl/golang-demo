abc22b97589f728c3438acd651c4c2fdf3dc82ad
kirago <zhangl910201@163.com>
2019-01-17 10:43:32 +0800

    添加信道示例

---
+++ GoTutorial/goroutinedemo/nobufferchan/main/main.go
@@ -0,0 +1,19 @@
+package nobufferchan
+
+import "fmt"
+
+var ch chan int = make(chan int)
+
+func foo(id int)  {
+	ch <- id
+}
+
+func main()  {
+	for i:=0;i<5;i++ {
+		go foo(i)
+	}
+
+	for i:=0;i<5;i++ {
+		fmt.Println(<-ch)
+	}
+}
